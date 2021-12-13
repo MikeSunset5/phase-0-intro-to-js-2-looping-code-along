126a2a171795aa98674e08bda90cd039ac46e99b
Michael Waldron <mikewaldron5@gmail.com>
2021-12-13 16:18:07 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names =["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    let note = [];
+    for (let i = 0; i < names.length; i++) {
+            let message = (`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+            note.push(message);
+        }
+
+    return note;
+}
+
+function countDown() {
+    let numbers = 10;
+    while (numbers >= 0) {
+        console.log (numbers);
+        numbers--    
+       
+    }
+    
+
+}
